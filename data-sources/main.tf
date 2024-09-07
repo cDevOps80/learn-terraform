82ebb305ecb16e144aafeacef1df1376e9b9fcba
chaithanya1812 <bob@example.com>
2024-09-07 23:05:45 +0530

    eks creation

--- data-sources/main.tf
+++ data-sources/main.tf
@@ -1,16 +1,28 @@
 # Fetching DevOps-Practice AMI-Id Information.
 
-data "aws_ami_ids" "ami" {
+#data "aws_ami_ids" "ami" {
+#
+#  owners = [973714476881]
+#  name_regex = "^*DevOps-Practice"
+#
+#  filter {
+#    name   = "block-device-mapping.volume-type"
+#    values = ["gp3"]
+#  }
+#}
+#
+#output "ami_list" {
+#  value = data.aws_ami_ids.ami
+#}
 
-  owners = [973714476881]
-  name_regex = "^*DevOps-Practice"
-
+data "aws_instance" "foo" {
   filter {
-    name   = "block-device-mapping.volume-type"
-    values = ["gp3"]
+    name   = "instance-state-name"
+    values = ["stopped"]
   }
+
 }
 
-output "ami_list" {
-  value = data.aws_ami_ids.ami
+output "aws_instances" {
+  value = data.aws_instance.foo
 }
