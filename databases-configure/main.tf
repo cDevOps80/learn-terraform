d60a2153c5df723571d53a567f22230de2209a76
chaithanya1812 <bob@example.com>
2024-09-08 22:41:36 +0530

    eks creation

---
+++ databases-configure/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "databases" {
+  ami           = "ami-0a5c3558529277641"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0665a56c7cd09a0e0"]
+  user_data = file("./user.sh")
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "stop"
+      spot_instance_type = "persistent"
+    }
+  }
+
+  tags = {
+    Name = "databases-1"
+  }
+
+}
