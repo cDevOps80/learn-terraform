88d45e735e07118ea1c12bde1c235e3edaf92845
chaithanya1812 <bob@example.com>
2024-09-08 09:58:47 +0530

    eks creation

--- data-sources/main.tf
+++ data-sources/main.tf
@@ -1,41 +1,43 @@
 # Fetching DevOps-Practice AMI-Id Information.
+/*
+data "aws_ami_ids" "ami" {
 
-#data "aws_ami_ids" "ami" {
-#
-#  owners = [973714476881]
-#  name_regex = "^*DevOps-Practice"
-#
-#  filter {
-#    name   = "block-device-mapping.volume-type"
-#    values = ["gp3"]
-#  }
-#}
-#
-#output "ami_list" {
-#  value = data.aws_ami_ids.ami
-#}
+  owners = [973714476881]
+  name_regex = "^*DevOps-Practice"
 
-#data "aws_instances" "foo" {
-#
-#  instance_state_names = ["stopped"]
-#
-#  instance_tags = {
-#    Name = "main"
-#  }
-#
-#
-#
-#}
-#
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp3"]
+  }
+}
+
+output "ami_list" {
+  value = data.aws_ami_ids.ami
+}
+*/
+
+/*
+data "aws_instances" "foo" {
+
+  instance_state_names = ["stopped"]
+
+  instance_tags = {
+    Name = "main"
+  }
+}
+*/
+
 #output "aws_instances" {
 #  value = data.aws_instances.foo
 #}
 
+/*
 data "external" "example" {
   working_dir = "/home/centos"
   program = ["bash","name"]
 }
-
-output "final" {
-  value = data.external.example.result["key1"]
-}
+*/
+#
+#output "final" {
+#  value = data.external.example.result["key1"]
+#}
