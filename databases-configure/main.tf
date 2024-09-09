61f9b5a4b5d000342773a3bb8f5234510486b9c5
chaithanya1812 <bob@example.com>
2024-09-09 09:02:12 +0530

    eks creation

--- databases-configure/main.tf
+++ databases-configure/main.tf
@@ -1,25 +1,54 @@
 
-resource "aws_instance" "databases" {
-  ami           = "ami-0a5c3558529277641"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.db-sg.id]
-  user_data = templatefile("./user.sh",{
-    MYSQL_ROOT_PASSWORD   = "RoboShop@1"
-    RABBITMQ_DEFAULT_USER = "roboshop"
-    RABBITMQ_DEFAULT_PASS = "roboshop123"
-  })
-  key_name = "nvirginia"
+#resource "aws_instance" "databases" {
+#  ami           = "ami-0a5c3558529277641"
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.db-sg.id]
+#  user_data = templatefile("./user.sh",{
+#    MYSQL_ROOT_PASSWORD   = "RoboShop@1"
+#    RABBITMQ_DEFAULT_USER = "roboshop"
+#    RABBITMQ_DEFAULT_PASS = "roboshop123"
+#  })
+#  key_name = "nvirginia"
+#
+#  instance_market_options {
+#    market_type = "spot"
+#    spot_options {
+#      instance_interruption_behavior = "stop"
+#      spot_instance_type = "persistent"
+#    }
+#  }
+#
+#  tags = {
+#    Name = "roboshop-db"
+#  }
+#}
 
-  instance_market_options {
-    market_type = "spot"
-    spot_options {
-      instance_interruption_behavior = "stop"
-      spot_instance_type = "persistent"
+variable "ports" {
+  default = {
+    ssh = {
+      port = 22
+      cidr_blocks = ["0.0.0.0/0"]
     }
-  }
-
-  tags = {
-    Name = "roboshop-db"
+    mysql = {
+      port = 3306
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    redis = {
+      port = 6379
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    mongod = {
+      port = 27017
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    rabbitmq = {
+      port = 5672
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    rabbitmq1 = {
+      port = 5671
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 }
 
@@ -28,49 +57,61 @@ resource "aws_security_group" "db-sg" {
   description = "Allow TLS inbound traffic "
   vpc_id      = data.aws_vpc.default.id
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+#  ingress {
+#    from_port        = 22
+#    to_port          = 22
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+
+  dynamic "ingress" {
+    for_each = var.ports
+    iterator = ports
+    content {
+      from_port        = ports.value.port
+      to_port          = ports.value.port
+      description      = "${ports.key}-allowing"
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
   }
 
-  ingress {
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+#  ingress {
+#    from_port        = 3306
+#    to_port          = 3306
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#
+#  }
+#
+#  ingress {
+#    from_port        = 27017
+#    to_port          = 27017
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#
+#  }
+#  ingress {
+#    from_port        = 6379
+#    to_port          = 6379
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#
+#  }
 
-  }
-
-  ingress {
-    from_port        = 27017
-    to_port          = 27017
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-  }
-  ingress {
-    from_port        = 6379
-    to_port          = 6379
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-  }
-
-  ingress {
-    from_port        = 5672
-    to_port          = 5672
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port        = 5671
-    to_port          = 5671
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    from_port        = 5672
+#    to_port          = 5672
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  ingress {
+#    from_port        = 5671
+#    to_port          = 5671
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
 
   egress {
     from_port        = 0
--- databases-configure/variable.tf
+++ databases-configure/variable.tf
@@ -2,10 +2,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-#variable "vpc_id" {
-#  default = data.aws_default_vpc.default.id
-#}
-
 output "vpc_id" {
   value = data.aws_vpc.default.id
 }
