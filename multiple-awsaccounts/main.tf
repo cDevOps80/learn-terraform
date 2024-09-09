89d1f7b0c52eb2d297ef13e459e2373656eb5e28
chaithanya1812 <bob@example.com>
2024-09-09 19:54:29 +0530

    terraform to deal with multiple aws accounts

---
+++ multiple-awsaccounts/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+provider "aws" {
+  alias = "apsouth"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "us-east" {
+  ami           = "ami-0a5c3558529277641"
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "us-east" {
+  provider = aws.apsouth
+  ami           = "ami-0888ba30fd446b771"
+  instance_type = "t2.micro"
+}
