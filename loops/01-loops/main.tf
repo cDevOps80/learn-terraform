6ac8d5cf2228f6afe3ebbab45cd5d4fffb5939ff
chaithanya1812 <bob@example.com>
2024-09-28 12:37:51 +0530

    for_each

--- loops/01-loops/main.tf
+++ loops/01-loops/main.tf
@@ -2,6 +2,13 @@ variable "instance_type" {
   default = ["t2.micro","t3.small","t3.medium",1]
 }
 
+output "values1" {
+  value = var.instance_type
+}
+
+output "values2" {
+  value = toset(var.instance_type)
+}
 
 resource "aws_instance" "web" {
   for_each = toset(var.instance_type)
