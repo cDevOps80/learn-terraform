398dcfea3b0934799aabe74287ac61edb6a2e4c9
chaithanya1812 <bob@example.com>
2024-09-28 12:13:07 +0530

    for_each

--- loops/main.tf
+++ loops/main.tf
@@ -24,6 +24,9 @@ output "instances" {
   value = { for key,ips in aws_instance.web: key => ips.public_ip }
 }
 
+output "instances-lists" {
+  value = [ for ips in aws_instance.web: ips.public_ip ]
+}
 output "instances-list" {
   value = aws_instance.web.*.public_ip
 }
