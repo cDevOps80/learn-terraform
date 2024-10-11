833a6485b026952f1bf91e25e0c6bbe3bde332b8
chaithanya1812 <bob@example.com>
2024-10-11 16:55:31 +0530

    terraform1

--- loops/02-loops/main.tf
+++ loops/02-loops/main.tf
@@ -18,7 +18,7 @@ locals {
 }
 
 output "one2" {
-  value = local.sample["one"].public_ip
+  value = [for key,ip in local.sample: key => ip.public_ip ]
 }
 output "one1" {
   value = aws_instance.sample
