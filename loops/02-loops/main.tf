ba3aef0193a6daa05fa637d03942a44acd08f798
chaithanya1812 <bob@example.com>
2024-10-11 17:06:31 +0530

    terraform1

--- loops/02-loops/main.tf
+++ loops/02-loops/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "sample" {
  # count         =  length(var.loop1)cd
   for_each       = toset(var.loop1)
   ami           = "ami-00f251754ac5da7f0"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
    Name = "web-${each.value}"
